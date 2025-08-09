c03f9bda2cda3a88eb193155d32cd4fe23ac9ff2
Hasnain Ashraf <161948837+Hasu121@users.noreply.github.com>
2025-08-09 20:16:31 +0600

    Update post.jsx

--- frontend/src/components/Post/post.jsx
+++ frontend/src/components/Post/post.jsx
@@ -1,18 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../Card/card";
-import { useState } from "react";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import CommentIcon from "@mui/icons-material/Comment";
 import SendIcon from "@mui/icons-material/Send";
+import axios from "axios";
+import { ToastContainer,toast } from "react-toastify";
 
 const Post = ({ profile, item, key, personalData }) => {
   const [seeMore, setSeeMore] = useState(false);
   const [comment, setComment] = useState(false);
 
-  const handleSendComment = (e) => {
+  const [comments, setComments] = useState([]);
+
+  const [liked, setLiked] = useState(false);
+  const [numOfLikes, setNumOfLikes] = useState(item?.likes.length || 0);
+
+  const [commentText, setCommentText] = useState("");
+
+  const handleSendComment = async (e) => {
     e.preventDefault();
+    if (commentText.trim().length === 0) {
+      return toast.error("Comment cannot be empty");
+    }
+
+    await axios.post(`http://localhost:4000/api/comment`,{postId:item?._id, comment:commentText},{withCredentials: true}).then((res) => {
+      setComments([res.data.comment,...comments]);
+      setCommentText("");
+    }).catch((err) => {
+      console.log(err);
+      toast.error("Error sending comment");
+    });
   };
-  console.log("item", item);
+
+  
+  useEffect(() => {
+    let selfId = personalData?._id;
+    item?.likes?.map((item) => {
+      if (item.toString() === selfId.toString()) {
+        setLiked(true);
+        return;
+      } else {
+        setLiked(false);
+      }
+    });
+  }, []);
+
+  const handleLikeFunc = async () => {
+    await axios
+      .post(
+        "http://localhost:4000/api/post/likeDislike",
+        { postId: item?._id },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        if (liked) {
+          setLiked(false);
+          setNumOfLikes((prev) => prev - 1);
+        } else {
+          setLiked(true);
+          setNumOfLikes((prev) => prev + 1);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error liking/disliking post");
+      });
+  };
+
+  const handleCommentBoxOpenClose = async () => {
+    setComment((prev) => !prev);
+    await axios
+      .get(`http://localhost:4000/api/comment/${item?._id}`)
+      .then((resp) => {
+        console.log(resp);
+        setComments(resp.data.comments);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error fetching comments");
+      });
+  };
 
   const desc = item?.desc;
   return (
@@ -53,10 +121,8 @@ const Post = ({ profile, item, key, personalData }) => {
 
       <div className="my-2 p-4 flex justify-between items-center">
         <div className="flex gap-1 items-center cursor-pointer">
-          <ThumbUpOffAltIcon sx={{ color: "blue", fontsize: 12 }} />
-          <span className="text-sm text-gray-500">
-            {item?.likes.length || 0} Likes
-          </span>
+          <ThumbUpIcon sx={{ color: "blue", fontsize: 12 }} />
+          <span className="text-sm text-gray-500">{numOfLikes} Likes</span>
         </div>
         <div className="flex gap-1 items-center cursor-pointer">
           <span className="text-sm text-gray-500">
@@ -67,12 +133,25 @@ const Post = ({ profile, item, key, personalData }) => {
 
       {!profile && (
         <div className="flex p-1">
-          <div className="w-[33%] justify-center flex gap-2 items-center border-r-1 border-gray-100 p-2 cursor-pointer hover:bg-gray-100">
-            {" "}
-            <ThumbUpOffAltIcon /> <span>Like</span>
+          <div
+            onClick={handleLikeFunc}
+            className="w-[33%] justify-center flex gap-2 items-center border-r-1 border-gray-100 p-2 cursor-pointer hover:bg-gray-100 group"
+          >
+            {liked ? (
+              <>
+                <ThumbUpIcon sx={{ color: "blue", fontsize: 22 }} />
+                <span className="group-hover:hidden">Liked</span>
+                <span className="hidden group-hover:inline">Unlike</span>
+              </>
+            ) : (
+              <>
+                <ThumbUpOffAltIcon sx={{ color: "blue", fontsize: 22 }} />
+                <span>Like</span>
+              </>
+            )}
           </div>
           <div
-            onClick={() => setComment(true)}
+            onClick={handleCommentBoxOpenClose}
             className="w-[33%] justify-center flex gap-2 items-center border-r-1 border-gray-100 p-2 cursor-pointer hover:bg-gray-100"
           >
             {" "}
@@ -91,11 +170,13 @@ const Post = ({ profile, item, key, personalData }) => {
           <div className="flex gap-2 items-center">
             <img
               className="w-12 h-12 rounded-full border-2 border-white object-cover"
-              src="https://www.parismatch.be/resizer/v2/QZXNSG5XVVCYHJNS5EZBZUL33Y.jpg?auth=fda480ffafedf925d1cf3fcf319a438d12a9723c8b4f9b0102fcda0f0890ef7e&width=1200&height=800&quality=85&focal=2689%2C1802"
+              src={personalData?.profile_pic || "https://via.placeholder.com/150" }
               alt=""
             />
             <form className="w-full flex gap-2" onSubmit={handleSendComment}>
               <input
+                value={commentText}
+                onChange={(event) => setCommentText(event.target.value)}
                 placeholder="Add a comment..."
                 className="w-full border-1 py-3 px-5 rounded-3xl hover:bg-gray-100"
               />
@@ -110,23 +191,28 @@ const Post = ({ profile, item, key, personalData }) => {
 
           {/*Others Comments List */}
           <div className="w-full p-4">
-            <div className="my-4">
-              <div className="flex gap-3">
-                <img
-                  className="w-10 h-10 rounded-full border-2 border-white object-cover"
-                  src="https://www.parismatch.be/resizer/v2/QZXNSG5XVVCYHJNS5EZBZUL33Y.jpg?auth=fda480ffafedf925d1cf3fcf319a438d12a9723c8b4f9b0102fcda0f0890ef7e&width=1200&height=800&quality=85&focal=2689%2C1802"
-                  alt=""
-                />
-                <div className="cursor-pointer">
-                  <div className="text-md font-semibold">Henry Cavill</div>
-                  <div className="text-xs text-gray-500">@Actor</div>
+            {comments.map((item, index) => {
+              return (
+                <div className="my-4">
+                  <div className="flex gap-3">
+                    <img
+                      className="w-10 h-10 rounded-full border-2 border-white object-cover"
+                      src={item?.user?.profile_pic || "https://via.placeholder.com/150"}
+                      alt=""
+                    />
+                    <div className="cursor-pointer">
+                      <div className="text-md font-semibold">{item?.user?.f_name || "Unknown User"}</div>
+                      <div className="text-xs text-gray-500">@{item?.user?.headline || "Unknown"}</div>
+                    </div>
+                  </div>
+                  <div className="px-11 my-2">{item?.comment}</div>
                 </div>
-              </div>
-              <div className="px-11 my-2">Hi this is cool</div>
-            </div>
+              );
+            })}
           </div>
         </div>
       )}
+      <ToastContainer />
     </Card>
   );
 };
